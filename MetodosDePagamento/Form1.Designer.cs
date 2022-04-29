70128778e01112b77394c8cc27860582ac431d12
Wesley Martins <wesmartins96@gmail.com>
2022-04-29 14:18:45 -0300

    Adicionar arquivos de projeto.

---
+++ MetodosDePagamento/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace MetodosDePagamento
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtValor = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.lbCodigoPix = new System.Windows.Forms.Label();
+            this.cbParcelas = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cbArray = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Valor";
+            // 
+            // txtValor
+            // 
+            this.txtValor.Location = new System.Drawing.Point(77, 33);
+            this.txtValor.Name = "txtValor";
+            this.txtValor.Size = new System.Drawing.Size(324, 28);
+            this.txtValor.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(158, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Formas de Pagamento";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 213);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Forma de Pagamento:";
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Location = new System.Drawing.Point(176, 213);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(36, 20);
+            this.lbResult.TabIndex = 0;
+            this.lbResult.Text = "0,00";
+            // 
+            // lbCodigoPix
+            // 
+            this.lbCodigoPix.AutoSize = true;
+            this.lbCodigoPix.BackColor = System.Drawing.Color.DarkCyan;
+            this.lbCodigoPix.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lbCodigoPix.Location = new System.Drawing.Point(27, 233);
+            this.lbCodigoPix.Name = "lbCodigoPix";
+            this.lbCodigoPix.Size = new System.Drawing.Size(56, 20);
+            this.lbCodigoPix.TabIndex = 0;
+            this.lbCodigoPix.Text = "codigo";
+            // 
+            // cbParcelas
+            // 
+            this.cbParcelas.FormattingEnabled = true;
+            this.cbParcelas.Location = new System.Drawing.Point(323, 144);
+            this.cbParcelas.Name = "cbParcelas";
+            this.cbParcelas.Size = new System.Drawing.Size(78, 28);
+            this.cbParcelas.TabIndex = 2;
+            this.cbParcelas.SelectedIndexChanged += new System.EventHandler(this.cbParcelas_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(280, 267);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(134, 75);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cbArray
+            // 
+            this.cbArray.FormattingEnabled = true;
+            this.cbArray.Location = new System.Drawing.Point(31, 144);
+            this.cbArray.Name = "cbArray";
+            this.cbArray.Size = new System.Drawing.Size(286, 28);
+            this.cbArray.TabIndex = 2;
+            this.cbArray.SelectedIndexChanged += new System.EventHandler(this.cbArray_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(414, 342);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cbArray);
+            this.Controls.Add(this.cbParcelas);
+            this.Controls.Add(this.lbCodigoPix);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtValor);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Ebrima", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtValor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.Label lbCodigoPix;
+        private System.Windows.Forms.ComboBox cbParcelas;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cbArray;
+    }
+}
+
